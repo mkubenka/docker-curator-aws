917a2edd443daf69bc1f518a0f23853d7e65605a
Michal Kubenka <mkubenka@gmail.com>
2017-10-05 17:23:31 +0200

    Fix failing tests. Because there is no foreground running service container was failing during the test.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,5 +11,7 @@ RSpec.configure do |c|
   c.before(:suite) do
     @image = Docker::Image.build_from_dir(project_root)
     set :docker_image, @image.id
+
+    set :docker_container_create_options, {'Cmd' => ['/bin/sh']}
   end
 end

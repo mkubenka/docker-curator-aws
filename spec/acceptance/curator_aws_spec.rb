ba227f4c54dd8a956e21e022bf9e86384f01a1f0
Michal Kubenka <mkubenka@gmail.com>
2017-10-05 17:22:53 +0200

    Test version output.

--- spec/acceptance/curator_aws_spec.rb
+++ spec/acceptance/curator_aws_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe 'curator_aws' do
-  describe command('/usr/bin/curator --help') do
+  describe command('/usr/bin/curator --version') do
     its(:exit_status) { should eq 0 }
+    its(:stdout) { should match /curator, version/ }
   end
 end
